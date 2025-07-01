a649ab026d1e81ab806064de1f3b8a2449e1bf49
scroutchy <stef.crombez@gmail.com>
2025-07-01 10:28:03 -0300

    feat: Init project

---
+++ src/main/kotlin/com/scr/btg/asynchronous/process/AsynchronousProcessApplication.kt
@@ -0,0 +1,11 @@
+package com.scr.btg.asynchronous.process
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsynchronousProcessApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsynchronousProcessApplication>(*args)
+}
---
+++ src/test/kotlin/com/scr/btg/asynchronous/process/AsynchronousProcessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scr.btg.asynchronous.process
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsynchronousProcessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
